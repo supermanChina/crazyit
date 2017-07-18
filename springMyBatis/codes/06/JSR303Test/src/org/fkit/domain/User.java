f9f0568773e7faf57d620e79f7702c4f00653e52
xiaochao.zhang <zhangxc96@163.com>
2017-07-18 20:08:42 +0800

    validator local run OK

--- springMyBatis/codes/06/JSR303Test/src/org/fkit/domain/User.java
+++ springMyBatis/codes/06/JSR303Test/src/org/fkit/domain/User.java
@@ -12,90 +12,84 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
-/**   
- * @Description: 
- * <br>网站：<a href="http://www.fkit.org">疯狂Java</a> 
- * @author 肖文吉	36750064@qq.com   
- * @version V1.0 
+/**
+ * @Description: <br>
+ * 				网站：<a href="http://www.fkit.org">疯狂Java</a>
+ * @author 肖文吉 36750064@qq.com
+ * @version V1.0
  * 
  */
 
-public class User implements Serializable{
-	
+public class User implements Serializable {
+	// 通过message_*.properties进行国际化错误消息显示
+	// 国际化消息key: 注解名.类名.属性名
+	// 或: 注解名.属性名
+	// 或: 注解名.属性类型名
+	// 或: 注解名
+	// 例如login属性的国际化key为：NotBlank.user.loginname
 	@NotBlank
 	private String loginname;
-	
+
 	@NotBlank
-	@Length(min=6,max=8)
+	@Length(min = 6, max = 8)
 	private String password;
-	
+
 	@NotBlank
 	private String username;
-	
-	@Range(min=15, max=60)
+
+	@Range(min = 15, max = 60)
 	private int age;
-	
+
 	@Email
 	private String email;
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Past
 	private Date birthday;
-	
-	@Pattern(regexp="[1][3,8][3,6,9][0-9]{8}")
-	private String phone;
 
+	@Pattern(regexp = "[1][3,8][3,6,9][0-9]{8}")
+	private String phone;
 
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-
 	public String getLoginname() {
 		return loginname;
 	}
-
 
 	public void setLoginname(String loginname) {
 		this.loginname = loginname;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	public int getAge() {
 		return age;
 	}
-
 
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getUsername() {
 		return username;
 	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
@@ -105,32 +99,22 @@ public class User implements Serializable{
 		return birthday;
 	}
 
-
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
-
 
 	public String getPhone() {
 		return phone;
 	}
 
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	@Override
 	public String toString() {
-		return "User [loginname=" + loginname + ", password=" + password
-				+ ", email=" + email + ", username="
-				+ username + ", birthDate=" + birthday + ", phone=" + phone
-				+ "]";
+		return "User [loginname=" + loginname + ", password=" + password + ", email=" + email + ", username=" + username
+				+ ", birthDate=" + birthday + ", phone=" + phone + "]";
 	}
 
-	
-	
-	
-	
 }
